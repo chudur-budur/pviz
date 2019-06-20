abaf9eac737ba6128e704599e994ebad46977371
chudur-budur <khaled.ahsan.talukder@gmail.com>
2019-06-19 22:48:57 -0400

    yo

--- src/dash-app/demo.py
+++ src/dash-app/demo.py
@@ -245,6 +245,29 @@ def create_layout(app):
                                             ),
                                         ],
                                     ),
+                                    html.Div([
+                                        dcc.Upload(
+                                            id='upload-data',
+                                            children=html.Div([
+                                                'Drag and Drop or ',
+                                                html.A('Select Files')
+                                            ]),
+                                            style={
+                                                'width': '100%',
+                                                'height': '60px',
+                                                'lineHeight': '60px',
+                                                'borderWidth': '1px',
+                                                'borderStyle': 'dashed',
+                                                'borderRadius': '5px',
+                                                'textAlign': 'center',
+                                                'margin': '10px'
+                                            },
+                                            # Allow multiple files to be uploaded
+                                            multiple=True
+                                        ),
+                                            
+                                            html.Div(id='output-data-upload'),
+                                    ]),
                                 ]
                             )
                         ],
