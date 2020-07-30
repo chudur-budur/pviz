3d76dc992ef87c29ac65cbda8c26f01dd7420d43
chudur-budur <khaled.ahsan.talukder@gmail.com>
2020-07-30 00:24:19 -0400

    looks like radviz is fine

--- vis/plotting.py
+++ vis/plotting.py
@@ -21,7 +21,9 @@ import matplotlib.colors as mc
 from matplotlib.patches import Circle
 from vis.utils import transform as tr
 
+
 __all__ = ["scatter", "camera_scatter", "radviz"]
+
 
 # Some good camera angles for scatter plots.
 camera_scatter = {
@@ -44,6 +46,7 @@ camera_scatter = {
     'gaa-nbi': {'10d': (-60,30)}
 }
 
+
 def scatter(A, plt, s = 1, c = mc.TABLEAU_COLORS['tab:blue'], **kwargs):
     r"""A scatter plot function.
 
@@ -62,7 +65,7 @@ def scatter(A, plt, s = 1, c = mc.TABLEAU_COLORS['tab:blue'], **kwargs):
 
     Other Parameters
     ----------------
-    label_prefiz : str, optional
+    label_prefix : str, optional
         The axis-label-prefix to be used, default `r"$f_{:d}$"` when optional.
     label_fontsize : str or int, optional
         The fontsize for the axes labels. Default `'large'` when optional.
@@ -83,7 +86,7 @@ def scatter(A, plt, s = 1, c = mc.TABLEAU_COLORS['tab:blue'], **kwargs):
     -------
     (fig, ax) : tuple of `matplotlib.pyplot.figure` and `matplotlib.axes.Axes`
         A tuple of `matplotlib.pyplot.figure` and `matplotlib.axes.Axes` 
-        (or `mpl_toolkits.mplot3d.axes.Acxes`) objects.
+        (or `mpl_toolkits.mplot3d.axes.Axes`) objects.
     """
     # all other parameters
     # by default label_prefix is $f_n$
@@ -131,25 +134,26 @@ def scatter(A, plt, s = 1, c = mc.TABLEAU_COLORS['tab:blue'], **kwargs):
     else:
         raise TypeError("A valid `matplotlib.pyplot` object must be provided.")
 
-def get_radviz_coordinates(X, factor = 1.0, normalized=True):
+
+def get_radviz_coordinates(X, spread_factor = 'auto', normalized=True):
     r"""Generate Radviz coordinates from data points `X`.
 
-    Maps all the data points in `X` (i.e. `|X| = n x m`) onto
-    Radviz coordinate positions. The `factor` parameter can be
-    used to "spread-out" the data points on Radviz. In higher
-    dimension, the points on Radviz tend to be clustered in the
-    center. We can increase this value to make the points more
-    visible.
+    Maps all the data points in `X` (i.e. `|X| = n x m`) onto radviz coordinate positions. 
+    The `factor` parameter can be used to "spread-out" the data points on Radviz. In higher
+    dimension, the points on Radviz tend to be clustered in the center. We can increase this 
+    value to make the points more visible.
 
     Parameters
     ----------
     X : ndarray 
         `n` number of `m` dim. points as input.
-    factor : float, optional
-        The "spread-factor" to make the points less cluttered in the 
-        center while dealing with high-dimensional data points. 
-        1.0 when default.
-    normalized : boolean, optional 
+    spread_factor: str {'auto'} or float, optional
+        The `spread_factor` might be needed if the data points are not sparse enough
+        in the original space. Setting this bigger than 1.0 will help the data points
+        to "spread-out" even more, while maintaining the placement of the points inside
+        the radviz anchor points. If `spread_factor = 'auto'`, the `factor` is 2.0 if 
+        `m > 3`, otherwise 1.0. Default `'auto'` when default.
+    normalized : bool, optional 
         If this value is True, then all the points in `X` will be 
         normalized within `[0.0, 1.0]`. True when default.
 
@@ -162,10 +166,21 @@ def get_radviz_coordinates(X, factor = 1.0, normalized=True):
         will be used to draw anchor points and the polygon.
     """
     m = X.shape[1]
+
+    # by default spread-factor is auto
+    if type(spread_factor) == str and spread_factor == 'auto':
+        factor = 2.0 if m > 3 else 1.0
+    elif type(spread_factor) == int or type(spread_factor) == float:
+        factor = spread_factor
+    else:
+        raise TypeError("`spread_factor` must be either 'auto' or a float/int.")
+    
+    # check if the data needs to be normalized
     if normalized:
         X_ = tr.normalize(X, lb = np.zeros(m), ub = np.ones(m))
     else:
         X_ = X
+
     T = 2 * np.pi * (np.arange(0, m, 1).astype(int) / m)
     COS = np.cos(T)
     SIN = np.sin(T)
@@ -180,7 +195,20 @@ def get_radviz_coordinates(X, factor = 1.0, normalized=True):
     K = np.column_stack((COS, SIN))
     return (P, K, B)
 
+
 def set_anchors(ax, A):
+    r"""Function to draw anchor points for radviz (and other related plots).
+
+    This function draws anchor points for radviz and other related plots
+    like Star-coordinate plots.
+
+    Parameters
+    ----------
+    ax : `matplotlib.axes.Axes` object
+        This object must be passed.
+    A : ndarray 
+        `m x 2` size of sin and cos values for the anchor coordinates.
+    """
     tgc = mc.TABLEAU_COLORS['tab:gray']
     for i in range(0, A.shape[0]-1):
         # draw one polygon line
@@ -194,7 +222,29 @@ def set_anchors(ax, A):
     # last pair of polygon points
     ax.scatter(A[-1,0], A[-1,1], c=tgc, marker='o', s=20.0, alpha=1.0)
 
-def set_anchor_labels(ax, A, label_prefix, label_fontsize, label_fontname, label_fontstyle):
+
+def set_anchor_labels(ax, A, label_prefix=r"$f_{:d}$", label_fontsize='large', 
+                        label_fontname=None, label_fontstyle=None):
+    r"""Function to put anchor labels for radviz and other related plots.
+
+    Function to draw anchor point labels for radivz and other related plots
+    like Star-coordinate plots.
+
+    Parameters
+    ----------
+    ax : `matplotlib.axes.Axes` object
+        This object must be passed.
+    A : ndarray 
+        `m x 2` size of sin and cos values for the anchor coordinates.
+    label_prefix : str, optional
+        The axis-label-prefix to be used, default `r"$f_{:d}$"` when optional.
+    label_fontsize : str or int, optional
+        The fontsize for the axes labels. Default `'large'` when optional.
+    label_fontname : str, optional
+        The fontname for the axes labels. Default `None` when optional.
+    label_fontstyle : str, optional
+        The fontstyle for the axes labels. Default `None` when optional.
+    """
     tgc = mc.TABLEAU_COLORS['tab:gray']
     # now put all the corner labels, like f1, f2, f3, ... etc.
     for xy, name in zip(A, [label_prefix.format(i+1) for i in range(A.shape[0])]):
@@ -216,7 +266,50 @@ def set_anchor_labels(ax, A, label_prefix, label_fontsize, label_fontname, label
 def radviz(A, plt, s = 1, c = mc.TABLEAU_COLORS['tab:blue'], \
             normalized=True, draw_axes=False, draw_anchors=True, \
             spread_factor='auto', **kwargs):
-    r"""
+    r"""A customized radviz plot.
+
+    This radviz plot is customized for the experiments. 
+    
+    Parameters
+    ----------
+    A : ndarray 
+        `n` number of `m` dim. points to be plotted.
+    plt : A `matplotlib.pyplot` object
+        It needs to be passed.
+    s : int or 1-D array_like, optional
+        Point size, or an array of point sizes. Default 1 when optional.
+    c : A `matplotlib.colors` object, str or an array RGBA color values.
+        Colors to be used. Default `mc.TABLEAU_COLORS['tab:blue']` when 
+        optional.
+    normalized : bool, optional
+        If needed, the data points in `A` can be normalized within `[0.0, 1.0]`. 
+        This helps to "spread-out" the data more on the radviz space. Default 
+        `True` when optional.
+    draw_axes: bool, optional
+        If `True`, the radviz plot will show axes. Default `False` when optional.
+    draw_anchors: bool, optional
+        If `False`, the radviz plot will hide anchors. Default `True` when optional.
+    spread_factor: str {'auto'} or float, optional
+        See `get_radviz_coordinates()` function for details.
+
+    Other Parameters
+    ----------------
+    label_prefix : str, optional
+        See `set_anchor_labels()` function for details.
+    label_fontsize : str or int, optional
+        See `set_anchor_labels()` function for details.
+    label_fontname : str, optional
+        See `set_anchor_labels()` function for details.
+    label_fontstyle : str, optional
+        See `set_anchor_labels()` function for details.
+    title : str, optional
+        The plot title. Default `None` when optional.
+
+    Returns
+    -------
+    (fig, ax) : tuple of `matplotlib.pyplot.figure` and `matplotlib.axes.Axes`
+        A tuple of `matplotlib.pyplot.figure` and `matplotlib.axes.Axes` 
+        (or `mpl_toolkits.mplot3d.axes.Axes`) objects.
     """
     # by default label_prefix is $f_n$
     label_prefix = kwargs['label_prefix'] if (kwargs is not None and 'label_prefix' in kwargs) \
@@ -233,29 +326,25 @@ def radviz(A, plt, s = 1, c = mc.TABLEAU_COLORS['tab:blue'], \
     # default plot title is empty
     title = kwargs['title'] if (kwargs is not None and 'title' in kwargs) else None
 
-    # by default spread-factor is auto
-    if type(spread_factor) == str and spread_factor == 'auto':
-        factor = 2.0 if A.shape[1] > 3 else 1.0
-    elif type(spread_factor) == int or type(spread_factor) == float:
-        factor = spread_factor
+    if plt is not None:
+        P, K, [lb, ub] = get_radviz_coordinates(A, spread_factor=spread_factor, normalized=normalized)
+        fig = plt.figure()
+        if title is not None:
+            fig.suptitle(title)
+        ax = fig.gca()
+        ax.scatter(P[:,0], P[:,1], s=s, c=c)
+        if draw_axes:
+            # ax.set_xticklabels([])
+            # ax.set_yticklabels([])
+            ax.set_axis_on()
+        else:
+            ax.set_axis_off()
+        if draw_anchors:
+            set_anchors(ax, K)
+            set_anchor_labels(ax, K, label_prefix, label_fontsize, label_fontname, label_fontstyle)
+        ax.set_xlim(lb[0] - 0.1 if lb[0] < -1 else -1.1, ub[0] + 0.01 if ub[0] > 1 else 1.1)
+        ax.set_ylim(lb[1] - 0.1 if lb[1] < -1 else -1.1, ub[1] + 0.01 if ub[1] > 1 else 1.1)
+        ax.set_aspect('equal')
+        return (fig, ax)
     else:
-        raise TypeError("`spread_factor` must be either 'auto' or a float/int.")
-
-    P, K, [lb, ub] = get_radviz_coordinates(A, factor=factor, \
-                                            normalized=normalized)
-    fig = plt.figure()
-    ax = fig.gca()
-    ax.scatter(P[:,0], P[:,1], s=s, c=c)
-    if draw_axes:
-        # ax.set_xticklabels([])
-        # ax.set_yticklabels([])
-        ax.set_axis_on()
-    else:
-        ax.set_axis_off()
-    if draw_anchors:
-        set_anchors(ax, K)
-        set_anchor_labels(ax, K, label_prefix, label_fontsize, label_fontname, label_fontstyle)
-    ax.set_xlim(lb[0] - 0.1 if lb[0] < -1 else -1.1, ub[0] + 0.01 if ub[0] > 1 else 1.1)
-    ax.set_ylim(lb[1] - 0.1 if lb[1] < -1 else -1.1, ub[1] + 0.01 if ub[1] > 1 else 1.1)
-    ax.set_aspect('equal')
-    return (fig, ax)
+        raise TypeError("A valid `matplotlib.pyplot` object must be provided.")
