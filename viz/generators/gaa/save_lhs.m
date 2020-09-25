929ef5dfb2cef72213ef1d97c08d8cd37179d128
chudur-budur <khaled.ahsan.talukder@gmail.com>
2020-09-24 22:57:31 -0400

    yo

--- viz/generators/gaa/save_lhs.m
+++
@@ -1,9 +0,0 @@
-% This script generates a a set of reference directions 
-% using LHS method and save them in a .mat file.
-
-clear;
-
-w = lhsdesign(3112, 10);
-% w = lhsdesign(6049, 10);
-filename = strcat('weights-lhs-', num2str(size(w,1)), '.mat');
-save(filename, 'w');
