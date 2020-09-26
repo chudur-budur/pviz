0422d52a6c5c93d8c2dbd04866cf172dd93de9dc
chudur-budur <khaled.ahsan.talukder@gmail.com>
2020-09-26 19:16:38 -0400

    new test_io

--- viz/utils/tests/test_io.py
+++ viz/utils/tests/test_io.py
@@ -6,9 +6,15 @@ from viz.utils import io
 class TestIo(unittest.TestCase):
     
     def test_is_number(self):
-        x = ['123', '123.4', '12.3.4', '-123.4', '123e10']
-        y = [io.is_number(v) for v in x]
-        self.assertEqual(y, [True, True, False, True, False])
+        tests = ['123', '12.3', '1.2.3', '-123', '-1.2.3', '-1.-2.-3', \
+                '12x3', '1e6', '1ex6', '1.2e6', '-1e6', '-1.1e6', '--1e6', \
+                 '1.2-e6', '1.2e-6', '1.2e-06', '1-2e6', '3.4e3', '34e-06', \
+                '3.4.2ee3', '1e-6', '1e+6', 'abc']
+        y = [io.is_number(v) for v in tests]
+        self.assertEqual(y, \
+                [True, True, False, True, False, False, False, True, False, \
+                True, True, True, False, False, True, True, False, True, True, 
+                False, True, False, False])
 
     def test_cast(self):
         x = [float, int, str, None]
